c618ed1e506a75cc964880c5efc512bc41fd478f
Eleonora <eleonora.kopiika@gmail.com>
2025-11-07 11:05:30 +0200

    step3: Implement AddEmployee page with employee form, formData state management, and onAddEmployee handler

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,39 @@
 import styles from './Header.module.css';
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
 	return (
 	  <header className={styles.header}>
 		 <h1 className={styles.logo}>HR App</h1>
+		 <nav className={styles.navMenu}>
+
+			<NavLink 
+			to="/" 
+			className={({isActive}) => 
+				`${styles.link} ${isActive ? styles.active : ""}`
+			} end
+			>Home
+			</NavLink> 
+
+			<NavLink
+          to="/add"
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.active : ""}`
+          }
+        >
+          Add Employee
+        </NavLink>
+
+			 <NavLink
+			 to="/about"
+				className={({ isActive }) =>
+					`${styles.link} ${isActive ? styles.active : ""}`
+				}
+			>
+				About
+			 </NavLink>
+			
+	    </nav>
 	  </header>
 	);
  };
